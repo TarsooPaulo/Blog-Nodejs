5b2d7c499d3c7a2dfd6dcf6b41097631c52fc065
TarsooPaulo <paulodetarse333@gmail.com>
2024-08-14 13:54:42 -0300

    some changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Carregando Módulos
 require("dotenv").config();
 const express = require("express");
-const exphbs = require("express-handlebars");
 const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 const app = express();
@@ -50,7 +49,7 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 // Handlebars
-app.engine("handlebars", exphbs());
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 // Mongoose
